ad4a987ea1d286d4978208465478ee645871b725
chaeseungwan <industry95@naver.com>
2022-07-05 21:19:16 +0900

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, request, redirect
-from data import data_load
-from model import scale_col_by_col, scale_col_by_row
 from sklearn.neighbors import NearestNeighbors
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
 app = Flask(__name__)
 
-sql_queery = 'SELECT * FROM beer_data'
-data = data_load(sql_queery)
-beer_style = data['beername'].unique()
-
-
+data = pd.read_csv('data/commadel_rating.csv')
+beer_style = data['Beer Name (Full)'].unique()
 
 @app.route('/')
 def home():
@@ -26,11 +24,25 @@ def result():
     else:
         return redirect('/')
     
-    data = data_load(sql_queery)
+    data = pd.read_csv('data/commadel_rating.csv')
     
     
-    tasting_profile_cols = ['astringency', 'body', 'alcohol', 'bitter', 'sweet', 'sour', 'salty', 'fruits', 'hoppy', 'spices', 'malty']
-    chem_cols = ['abv', 'minibu', 'maxibu']
+    tasting_profile_cols = ['Astringency', 'Body', 'Alcohol', 'Bitter', 'Sweet', 'Sour', 'Salty', 'Fruits', 'Hoppy', 'Spices', 'Malty']
+    chem_cols = ['ABV', 'Min IBU', 'Max IBU']
+    
+    def scale_col_by_row(df, cols):
+        scaler = MinMaxScaler()
+        # Scale values by row
+        scaled_cols = pd.DataFrame(scaler.fit_transform(df[cols].T).T, columns=cols)
+        df[cols] = scaled_cols
+        return df
+
+    def scale_col_by_col(df, cols):
+        scaler = MinMaxScaler()
+        # Scale values by column
+        scaled_cols = pd.DataFrame(scaler.fit_transform(df[cols]), columns=cols)
+        df[cols] = scaled_cols
+        return df
         
         
     # Scale values in tasting profile features (across rows)
@@ -42,23 +54,22 @@ def result():
     # Scale values in chemical features (across columns)
     data = scale_col_by_col(data, chem_cols)
 
-    df = data.drop(['name', 'description', 'review_aroma', 'review_appearance', 'review_palate', 'review_taste', 'review_overall', 'number_of_reviews'],axis=1)
+    df = data.drop(['Name', 'Description', 'review_aroma', 'review_appearance', 'review_palate', 'review_taste', 'review_overall', 'number_of_reviews'],axis=1)
 
     # Use only numeric features
     df_num = df.select_dtypes(exclude=['object'])
 
     # user_input = select # user가 입력한 값
-
-    test_data = data[data['beername'] == str(select)]
+    test_data = data[data['Beer Name (Full)'] == str(select)]
     num_input = df_num.loc[test_data.index].values
 
     def get_neighbors(data, num_input, style_input, same_style=False):
         if same_style==True:
             # Locate beers of same style
-            df_target = data[data["style"] == style_input].reset_index(drop=True)
+            df_target = data[data["Style"] == style_input].reset_index(drop=True)
         else:
             # Locate beers of different styles
-            df_target = data[data["style"] != style_input].reset_index(drop=True)
+            df_target = data[data["Style"] != style_input].reset_index(drop=True)
 
         df_target_num = df_num.loc[df_target.index]
         # Calculate similarities (n_neighbors=6 for 5 recommendations)
@@ -67,14 +78,14 @@ def result():
         # Top 5 recommendations
         target_rec_df = df_target.loc[queried_indices[0][1:]]
         target_rec_df = target_rec_df.sort_values(by=['review_overall'], ascending=False)
-        target_rec_df = target_rec_df[['name', 'brewery', 'style', 'review_overall']]
+        target_rec_df = target_rec_df[['Name', 'Brewery', 'Style', 'review_overall']]
         target_rec_df.index = range(1, 6)
         target_rec_df.drop('review_overall', axis=1, inplace=True)
         return target_rec_df
 
 
     # Detect beer style
-    style_input = test_data['style'].iloc[0]
+    style_input = test_data['Style'].iloc[0]
     top_5_same_style_rec = get_neighbors(data, num_input, style_input, same_style=True)
     top_5_false_style_rec = get_neighbors(data, num_input, style_input, same_style=False)
         
